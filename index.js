2a138182aea307519b93e209a380d75939be94cc
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-29 22:54:51 +0200

    update home page style

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(session({
         expires: 600000
     }
 }));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
--- src/controller/Admin/home_controller.js
+++ src/controller/Admin/home_controller.js
@@ -3,27 +3,24 @@ const session = require('express-session');
 const methods = {
     async getHome(req, res, next) {
         try {
-            const carsouel_data = [
+            const cards_data = [
                 {
-                    active: '',
                     title: 'Manage Your Business',
-                    description: 'make account account easily you can add you products',
-                    img: 'https://images.unsplash.com/photo-1591696205602-2f950c417cb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                    description: 'we manageing your products and we push them to our online shopping site and we use the most advanced technologies to manage sites',
+                    img: 'assets/svg/svg1.svg',
                 },
                 {
-                    active: 'active',
-                    title: 'offers',
-                    description: 'There is easy way to offer your products',
-                    img: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1544&q=80',
+                    title: 'objective',
+                    description: 'to manage products from all aspects to know all information about your product views or how many customers who bought it',
+                    img: 'assets/svg/svg2.svg',
                 },
                 {
-                    active: '',
                     title: 'make money',
-                    description: 'just get started with online shopping',
-                    img: 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+                    description: 'just get started with online shopping and let us manage your products in easy ways',
+                    img: 'assets/svg/svg3.svg',
                 }
             ]
-            res.render("Admin/Home/home.ejs", { carsouel_data: carsouel_data, admin: req?.session?.admin });
+            res.render("Admin/Home/home.ejs", { cards_data: cards_data, admin: req?.session?.admin });
         } catch (error) {
             next(createError(error.status, error.message));
         }
