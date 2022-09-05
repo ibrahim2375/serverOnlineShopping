0a5f7685168eba2b78e1920f9fc86756bd3bc517
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-09-06 00:49:35 +0200

    update payment  & order

--- src/controller/user/makeOrder_controller.js
+++ src/controller/user/makeOrder_controller.js
@@ -3,15 +3,29 @@ const createError = require('../../errors/errorHandle');
 const methods = {
     async makeOrder(req, res, next) {
         try {
-            const newOrder = new Order({ ...req.body, userId: req.user.id });
-            await newOrder.save().then((result) => {
-                if (!result) {
-                    res.status(200).send('not added succesfully....');
-                }
-                res.status(200).json(result);
-            }).catch((err) => {
-                next(createError(err.status, err.message));
-            });
+            const { orders } = req.body;
+             orders?.map(async (order) => {
+                const newOrder = new Order({
+                    productId: order._id,
+                    size: order.size,
+                    color: order.color,
+                    name: order.name,
+                    price: order.price,
+                    quantity: order.quantity,
+                    img: order.img,
+                    productId: order._id,
+                    userId: req.user.id
+                });
+                await newOrder.save().then((result) => {
+                    if (!result) {
+                        res.status(200).send('not added succesfully....');
+                    }
+                    res.status(200).send('added succesfully....');
+                }).catch((err) => {
+                    res.status(403);
+                });
+
+            })
 
         } catch (error) {
             next(createError(error.status, error.message));
--- src/controller/user/payments/payments_controller.js
+++ src/controller/user/payments/payments_controller.js
@@ -1,77 +1,25 @@
 const Purchase = require('../../../../models/purchase');
 const Stripe = require('stripe')(process.env.S_K);
-const { v8: uuidv8 } = require('uuid');
+// const { v4 } = require('uuid');
 const createError = require('../../../errors/errorHandle');
 const methods = {
     async paymentMethod(req, res, next) {
         try {
-            const { totalPrice, token, basketItems } = req.body;
-            PaymentKey = uuidv8();
-            Stripe.customers
-                .create({
-                    email: token.email,
-                    source: token.id,
+            // const { totalPrice, token, orders } = req.body;
+            await Stripe.charges.create(
+                {
+                    source: req.body.tokenId,
+                    amount: req.body.totalPrice * 100,
+                    currency: "usd",
+                },
+                (stripeErr, stripeRes) => {
+                    if (stripeErr) {
+                        res.status(500).json(stripeErr);
+                    } else {
+                        res.status(200).json(stripeRes);
+                    }
                 })
-                .then((customer) => {
-                    // have access to the customer object
-                    return Stripe.invoiceItems
-                        .create({
-                            customer: customer.id, // set the customer id
-                            amount: totalPrice * 100,// 25
-                            currency: 'usd',
-                            description: `products ${basketItems}`,
-                            shipping: {
-                                name: token.card.name,
-                                address: {
-                                    country: token.card.address_country,
-                                }
-                            }
-                        }).then((invoiceItem) => {
-                            return Stripe.invoices.create({
-                                collection_method: 'send_invoice',
-                                customer: invoiceItem.customer,
-                            });
-                        })
-                        .then((invoice) => {
-                            res.status(200).json(invoice);
-                        })
-                        .catch((err) => {
-                            res.status(403).json(err);
-                        });
-                });
-            // Stripe.customers.create({
-            //     email: token.email,
-            //     source: token.id,
-            // }).then((customer) => {
-            //     // have access to the customer object
-            //     Stripe.charges.create({
-            //         amount: totalPrice * 100,
-            //         currency: 'usd',
-            //         customer: customer.id,
-            //         receipt_email: token.email,
-            //         description: `purchase of products`,
-            //         shipping: {
-            //             name: token.card.name,
-            //             address: {
-            //                 country: token.card.address_country,
-            //             }
-            //         }
 
-            //     }, { PaymentKey })
-
-            // }).then(async (result) => {
-            //     res.status(200).json(result);
-
-            // }).catch((err) => {
-            //     res.status(403).json(err);
-            // });
-
-            // await basketItems.map((item) => {
-            //     const newPurchases = new Purchase({ userId: item?.userId, productId: item?.productId, quantity: item?.quantity, price: item?.price });
-            //     newPurchases.save().then((result) => {
-            //         res.status(200).json(result);
-            //     }).catch((err) => { console.log(err) })
-            // })
         } catch (error) {
             next(createError(error.status, error.message));
         }
