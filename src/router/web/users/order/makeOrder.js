4de6a1fc313b71b5473036f0ed8255a89041a0b6
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-09-06 04:35:00 +0200

    rm user auth from makeorder tst

--- src/router/web/users/order/makeOrder.js
+++ src/router/web/users/order/makeOrder.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateUser = require('../../../../../middlewares/authenticateUser');
+// const authenticateUser = require('../../../../../middlewares/authenticateUser');
 const makeOrder_controller = require('../../../../controller/user/makeOrder_controller')
-router.post('/', authenticateUser, makeOrder_controller.makeOrder);
+router.post('/', makeOrder_controller.makeOrder);
 module.exports = router;
